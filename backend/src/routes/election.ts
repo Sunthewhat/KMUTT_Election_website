18326a98945f3560d9e557a93a772bbf9c3127e2
Paramita Saenghao <judge.27miyoo@gmail.com>
2024-03-08 21:30:10 +0700

    Revert "Merge remote-tracking branch 'origin/Mink' into Judge"
    This reverts commit e1a0eb58a22da7db5cb14bd97c2a44c0f7a6881e, reversing
    changes made to 094509b12ab2e210b87a0f3d91b4db6d60aaee36.

--- backend/src/routes/election.ts
+++ backend/src/routes/election.ts
@@ -1,17 +1,7 @@
-import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
 
-const electionRoute = new Hono();
-const prisma = new PrismaClient();
+const app = new Hono();
 
-electionRoute.get("/geteligible", async (c) => {
-  const eligibleList = await prisma.eligible.findMany({
-    select: {
-      id: true,
-      firstname: true,
-    },
-  });
-  return c.json(eligibleList);
-});
-
-export default electionRoute;
+app.get("/", (c) => {
+    return c.text("Hello");
+})
--- backend/src/routes/eligible.ts
+++ backend/src/routes/eligible.ts
@@ -4,38 +4,12 @@ import { Hono } from "hono";
 const eligibleRoute = new Hono();
 const prisma = new PrismaClient();
 
-// eligibleRoute.get("/geteligible", async (c) => {
-//   const eligibleList = await prisma.eligible.findMany({
-//     select: {
-//       id: true,
-//       firstname: true,
-//     },
-//   });
-//   return c.json(eligibleList);
-// });
-
-eligibleRoute.get("/check-rights", async (c) => {
-  const isEligible = await prisma.eligible.findUnique({
-    where: {
-      id: 65130500205,
-    },
-    select: {
-      student_id: true,
-      prefix: true,
-      firstname: true,
-      lastname: true,
-    },
-  });
-  if (isEligible) {
-    return c.json({ isEligible: true, ...isEligible });
-  } else {
-    return c.json({
-      isEligible: false,
-      prefix: "",
-      firstname: "",
-      lastname: "",
-    });
-  }
+eligibleRoute.get("/geteligible", async (c) => {
+    const eligibleList = await prisma.eligible.findMany({select: {
+        id: true,
+        firstname: true
+    }})
+    return c.json(eligibleList)
 });
 
 export default eligibleRoute;
