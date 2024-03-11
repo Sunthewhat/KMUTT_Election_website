9802d6831ab7f7a1d0f4885de3669ed88adef63a
paphachanok <108296059+paphachanok@users.noreply.github.com>
2024-03-11 21:37:28 +0700

    [FIX] party-info

--- public/src/app/party-info/page.tsx
+++ public/src/app/party-info/page.tsx
@@ -1,38 +1,35 @@
+"use client";
+
 import { PartyBigCard } from "@/components/candidate_info/party_big_card";
 import { PartySmallCard } from "@/components/candidate_info/party_small_card";
 import { Title } from "@/components/title";
 import { Flex, Row, Col, Grid } from "antd";
+import React, { useState } from "react";
 
 const MockData = [
   {
     partyName: "ก้าวไกล",
-    partyNo: "1",
-	partyLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Move_Forward_Party_Logo.svg/1200px-Move_Forward_Party_Logo.svg.png"
+    partyNo: 1,
+    partyLogo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Move_Forward_Party_Logo.svg/1200px-Move_Forward_Party_Logo.svg.png",
   },
   {
     partyName: "ก้าวไกล",
-    partyNo: "2",
-	partyLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Move_Forward_Party_Logo.svg/1200px-Move_Forward_Party_Logo.svg.png"
+    partyNo: 2,
+    partyLogo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Move_Forward_Party_Logo.svg/1200px-Move_Forward_Party_Logo.svg.png",
   },
   {
     partyName: "ก้าวไกล",
-    partyNo: "3",
-	partyLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Move_Forward_Party_Logo.svg/1200px-Move_Forward_Party_Logo.svg.png"
+    partyNo: 3,
+    partyLogo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Move_Forward_Party_Logo.svg/1200px-Move_Forward_Party_Logo.svg.png",
   },
   {
     partyName: "ก้าวไกล",
-    partyNo: "4",
-	partyLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Move_Forward_Party_Logo.svg/1200px-Move_Forward_Party_Logo.svg.png"
-  },
-  {
-    partyName: "ก้าวไกล",
-    partyNo: "5",
-	partyLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Move_Forward_Party_Logo.svg/1200px-Move_Forward_Party_Logo.svg.png"
-  },
-  {
-    partyName: "ก้าวไกล",
-    partyNo: "6",
-	partyLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Move_Forward_Party_Logo.svg/1200px-Move_Forward_Party_Logo.svg.png"
+    partyNo: 4,
+    partyLogo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Move_Forward_Party_Logo.svg/1200px-Move_Forward_Party_Logo.svg.png",
   },
 ];
 
@@ -43,27 +40,27 @@ const MockBigCard = [
     leader: "ประยุทธ์  จันทร์โอชา",
     leader_image:
       "https://pbs.twimg.com/profile_images/1201483636454744065/SOwgoBYn_400x400.jpg",
-	  question: ["Question 1", "Question 2", "Question 3", "Question 4"],
-	  answer: [
-		  ["Answer 11", "Answer 12", "Answer 13"],
-		  ["Answer 21", "Answer 22", "Answer 23"],
-		  ["Answer 31", "Answer 32", "Answer 33"],
-		  ["Answer 41", "Answer 42", "Answer 43"],
-	  ]
+    question: ["Question 1", "Question 2", "Question 3", "Question 4"],
+    answer: [
+      ["Answer 11", "Answer 12", "Answer 13"],
+      ["Answer 21", "Answer 22", "Answer 23"],
+      ["Answer 31", "Answer 32", "Answer 33"],
+      ["Answer 41", "Answer 42", "Answer 43"],
+    ],
   },
   {
-	partyNo: 2,
-	name: "พลังประชารัฐ",
-	leader: "ประยุทธ์  จันทร์โอชา",
-	leader_image:
-	  "https://pbs.twimg.com/profile_images/1201483636454744065/SOwgoBYn_400x400.jpg",
-	  question: ["Question 1", "Question 2", "Question 3", "Question 4"],
-	  answer: [
-		  ["Answer 11", "Answer 12", "Answer 13"],
-		  ["Answer 21", "Answer 22", "Answer 23"],
-		  ["Answer 31", "Answer 32", "Answer 33"],
-		  ["Answer 41", "Answer 42", "Answer 43"],
-	  ]
+    partyNo: 2,
+    name: "พลังประชารัฐ",
+    leader: "ประยุทธ์  จันทร์โอชา",
+    leader_image:
+      "https://pbs.twimg.com/profile_images/1201483636454744065/SOwgoBYn_400x400.jpg",
+    question: ["Question 1", "Question 2", "Question 3", "Question 4"],
+    answer: [
+      ["Answer 11", "Answer 12", "Answer 13"],
+      ["Answer 21", "Answer 22", "Answer 23"],
+      ["Answer 31", "Answer 32", "Answer 33"],
+      ["Answer 41", "Answer 42", "Answer 43"],
+    ],
   },
   {
     partyNo: 3,
@@ -71,17 +68,39 @@ const MockBigCard = [
     leader: "ประยุทธ์  จันทร์โอชา",
     leader_image:
       "https://pbs.twimg.com/profile_images/1201483636454744065/SOwgoBYn_400x400.jpg",
-	question: ["Question 1", "Question 2", "Question 3", "Question 4"],
-	answer: [
-		["Answer 11", "Answer 12", "Answer 13"],
-		["Answer 21", "Answer 22", "Answer 23"],
-		["Answer 31", "Answer 32", "Answer 33"],
-		["Answer 41", "Answer 42", "Answer 43"],
-	]
+    question: ["Question 1", "Question 2", "Question 3", "Question 4"],
+    answer: [
+      ["Answer 11", "Answer 12", "Answer 13"],
+      ["Answer 21", "Answer 22", "Answer 23"],
+      ["Answer 31", "Answer 32", "Answer 33"],
+      ["Answer 41", "Answer 42", "Answer 43"],
+    ],
+  },
+  {
+    partyNo: 4,
+    name: "พลังประชารัฐ",
+    leader: "ประยุทธ์  จันทร์โอชา",
+    leader_image:
+      "https://pbs.twimg.com/profile_images/1201483636454744065/SOwgoBYn_400x400.jpg",
+    question: ["Question 1", "Question 2", "Question 3", "Question 4"],
+    answer: [
+      ["Answer 11", "Answer 12", "Answer 13"],
+      ["Answer 21", "Answer 22", "Answer 23"],
+      ["Answer 31", "Answer 32", "Answer 33"],
+      ["Answer 41", "Answer 42", "Answer 43"],
+    ],
   },
 ];
 
 const TestPage = () => {
+  const firstPartyNo = MockData.length > 0 ? MockData[0].partyNo : null;
+  const [selectedParty, setSelectedParty] = useState<number | null>(
+    firstPartyNo
+  );
+
+  const handleSelectParty = (partyNo: number) => {
+    setSelectedParty(partyNo);
+  };
   return (
     <Flex style={{ flexDirection: "column", margin: "0 auto" }}>
       <Title
@@ -90,13 +109,23 @@ const TestPage = () => {
       />
       <Flex style={{ flexDirection: "row", overflow: "auto" }} gap={10}>
         {MockData.map((data) => (
-          <PartySmallCard key={data.partyNo} {...data} />
+          <PartySmallCard
+            key={data.partyNo}
+            partyName={data.partyName}
+            partyNo={data.partyNo}
+            partyLogo={data.partyLogo}
+            isSelected={selectedParty === data.partyNo}
+            onSelect={() => handleSelectParty(data.partyNo)}
+          />
         ))}
       </Flex>
       <Flex style={{ flexDirection: "column", marginTop: "10px" }} gap={10}>
-        {MockBigCard.map((data) => (
-          <PartyBigCard key={data.partyNo} {...data} />
-        ))}
+        {selectedParty !== null && (
+          <PartyBigCard
+            key={selectedParty}
+            {...MockBigCard[selectedParty - 1]}
+          />
+        )}
       </Flex>
     </Flex>
   );
--- public/src/components/candidate_info/party_small_card.tsx
+++ public/src/components/candidate_info/party_small_card.tsx
@@ -1,28 +1,47 @@
 import { Card, Flex } from "antd";
+import React, { useState } from "react";
 
 interface PartySmallCardProps {
-	partyName?: string;
-	partyNo?: string;
-	partyLogo?: string;
+  partyName?: string;
+  partyNo?: number;
+  partyLogo?: string;
+  isSelected: boolean;
+  onSelect: () => void;
 }
 
-export const PartySmallCard = ({partyName, partyNo, partyLogo}: PartySmallCardProps) => {
-	return (
-		<Card style={{
-			minWidth: "140px",
-			height: "81px",
-			borderRadius: "0",
-			backgroundImage: `url(${partyLogo})`,
-			backgroundSize: "45px auto",
-            backgroundPosition: "center",
-			backgroundRepeat: "no-repeat",
-		}}>
-			<Flex style={{flexDirection: "row"}} justify="center" align="flex-end" gap={5}>
-				<Flex style={{flexDirection: "column"}}>
-					<p>{partyName}</p>
-				</Flex>
-				<h5>{partyNo}</h5>
-			</Flex>
-		</Card>
-	);
+export const PartySmallCard = ({
+  partyName,
+  partyNo,
+  partyLogo,
+  isSelected,
+  onSelect,
+}: PartySmallCardProps) => {
+  return (
+    <Card
+      style={{
+        minWidth: "140px",
+        height: "81px",
+        borderRadius: "0",
+        margin: "1em",
+        backgroundImage: `url(${partyLogo})`,
+        backgroundSize: "45px auto",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        boxShadow: isSelected ? "0px 0px 5px 5px orange" : "none",
+      }}
+      onClick={onSelect}
+    >
+      <Flex
+        style={{ flexDirection: "row" }}
+        justify="center"
+        align="flex-end"
+        gap={5}
+      >
+        <Flex style={{ flexDirection: "column" }}>
+          <p>{partyName}</p>
+        </Flex>
+        <h5>{partyNo}</h5>
+      </Flex>
+    </Card>
+  );
 };
